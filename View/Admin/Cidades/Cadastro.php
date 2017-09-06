01eebb9adb62612398538e915b034dbf55c98dc9
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2017-09-06 20:19:20 -0300

    Objetos básicos cadastrados
    Objetos básicos de CRUD de estado e cidade foram criados

---
+++ View/Admin/Cidades/Cadastro.php
@@ -0,0 +1,15 @@
+<?php
+    
+?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <h1>Cadastrar Cidade</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        
+                    </div>
+                </div>
+            </div>
