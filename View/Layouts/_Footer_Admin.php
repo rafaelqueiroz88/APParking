6e8817f9bf9478b85949ae7053a42fdbedd8fb38
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2017-09-06 22:51:28 -0300

    Menu Admin
    Manutenção nos links do menu e no rodapé

--- View/Layouts/_Footer_Admin.php
+++ View/Layouts/_Footer_Admin.php
@@ -6,9 +6,29 @@
 
 		</main>
 		<footer>
-			<time>
-				<h2> <?php $home->VerData(); ?> </h2>
-			</time>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12">
+						<p>
+							<h2> GreS Software </h2>
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12">
+						<time>
+							<h3> <?php $home->VerData(); ?> </h3>
+						</time>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-4">
+						<p>
+							&copy; - Todos os direitos reservados
+						</p>
+					</div>
+				</div>
+			</div>
 		</footer>
 		<script type="text/javascript" src="./Public/js/jquery-3.2.1.js"></script>
 		<script type="text/javascript" src="./Public/js/jquery.mask.js"></script>
